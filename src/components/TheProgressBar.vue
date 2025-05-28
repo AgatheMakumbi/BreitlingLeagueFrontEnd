b5703354d7ea1b93f35a2facee0615cb1c81ea60
agathe makumbi <makagathe7@gmail.com>
2025-05-28 12:42:23 +0200

    component ProgressBar created

---
+++ src/components/TheProgressBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="progressBarWrapper">
+      <div class="circular-bar">
+        <div class="percent">0%</div>
+      </div>
+      <label class="progressLabel">Novelties</label>
+    </div>
+</template>
+<script setup>
+import { onMounted, ref } from "vue";
+// javascript for circular progress bar
+onMounted(() => {
+  const circularBar = document.querySelector(".circular-bar");
+  const percent = document.querySelector(".percent");
+
+  const initialValue = ref(0); // Initial value for the progress
+  const currentValue = ref(70);
+  const speed = 10; // Adjust the speed of the progress
+
+  //increase the value each 10ms then update the progress and text content
+  const timer = setInterval(() => {
+    initialValue.value++;
+    console.log(initialValue.value);
+
+    const degree = (initialValue.value / 100) * 360;
+    circularBar.style.background = `conic-gradient(#ffc72c ${degree}deg, #ffe08d 0deg)`;
+    percent.textContent = `${initialValue.value}%`;
+
+    if (initialValue.value >= currentValue.value) {
+      clearInterval(timer);
+    }
+  }, speed);
+});
+</script>
--- src/components/pages/BreitlingLeague.vue
+++ src/components/pages/BreitlingLeague.vue
@@ -1,40 +1,52 @@
-<script setup>
-import { ref } from 'vue'
-const challenges = ref([])
-
-//fetch http://127.0.0.1:8000/api/challenges 
-// to get the challenges and display them in the template
-// Fetch challenges and update the ref
-const fetchChallenges = async () => {
-  try {
-    const response = await fetch('http://127.0.0.1:8000/api/challenges')
-    const data = await response.json()
-    challenges.value = data // Update the ref value
-    console.log('Challenges fetched successfully:', challenges.value)
-  } catch (error) {
-    console.error('Error fetching challenges:', error)
-  }
-}
-
-// Call the fetch function
-fetchChallenges()
-</script>
-
 <template>
-  <p class="greetingBreitling">breitling League</p>
-  
-    <div>
+  <section href="#home" class="home">
+    <h1 class="pageTitle">Breitling League</h1>
+
+    <p>
+      The Breitling League is a competition that allows you to challenge your
+      friends and other players in various challenges.
+    </p>
+
+    <TheProgressBar />
+
+    <h1 class="pageTitle">Competition</h1>
+    <p>
+      The Breitling League is a competition that allows you to challenge your
+      friends and other players in various challenges.
+    </p>
+  </section>
+
+  <!-- <div>
         <ul>
         <li v-for="challenge in challenges" :key="challenge.id">
             {{ challenge.titre }} - {{ challenge.contenu }}
         </li>
         </ul>
-    </div>
+    </div> -->
 </template>
 
-<style>
-.greetingBreitling {
-  color: rgb(255, 217, 0);
-  font-weight: bold;
-}
-</style>        
+<script setup>
+import { onMounted, ref } from "vue";
+import TheProgressBar from "@/components/TheProgressBar.vue";
+const challenges = ref([]);
+
+//fetch http://127.0.0.1:8000/api/challenges
+// to get the challenges and display them in the template
+// Fetch challenges and update the ref
+const fetchChallenges = async () => {
+  try {
+    const response = await fetch("http://127.0.0.1:8000/api/challenges");
+    const data = await response.json();
+    challenges.value = data; // Update the ref value
+    console.log("Challenges fetched successfully:", challenges.value);
+  } catch (error) {
+    console.error("Error fetching challenges:", error);
+  }
+};
+
+// Call the fetch function
+//fetchChallenges();
+
+</script>
+
+<style></style>

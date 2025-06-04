01eefc731c6275ca583034f390ce41f55efa99c6
vecval24 <valentine.vecchi@heig-vd.ch>
2025-06-04 10:54:02 +0200

    onboarding page terminee

--- src/components/LeagueSubPages/OnBoarding.vue
+++ src/components/LeagueSubPages/OnBoarding.vue
@@ -1,27 +1,24 @@
-<template >
-   <div class="heroWrapper centered">
-      <h1 class="pageTitleHero">On <span>boarding</span></h1>
+<template>
+    <div class="heroWrapper centered">
+        <h1 class="pageTitleHero">On <span>boarding</span></h1>
 
-      <p class="pageDescriptionHero">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui
-      </p>
-   </div>
-   <div>
-    
-   </div>
-   <div class="centered">
-    <TheButtonPrimary label-name="league"/>
-   </div>
+        <p class="pageDescriptionHero">
+            Before entering the League, we invite you to watch the onboarding video. This short introduction gives you a
+            clear overview of how the Breitling League works. It’s the perfect starting point to understand the rules
+            and how to make the most of your experience. Watching it will ensure you’re fully prepared to dive in, start
+            learning, and compete with confidence.
+        </p>
+    </div>
+    <div class="centered">
+        <img class="w-80 h-48 mt-[50px]" src="/src/assets/videoimg.jpg" />
+    </div>
+    <div class="centered mt-[40px] mb-[77px]">
+        <TheButtonPrimary label-name="league" />
+    </div>
 
 
 </template>
 <script setup>
-   import TheButtonPrimary from '@/components/TheButtonPrimary.vue';
+import TheButtonPrimary from '@/components/TheButtonPrimary.vue';
 </script>
-<style setup>
-    
-</style>
+<style setup></style>

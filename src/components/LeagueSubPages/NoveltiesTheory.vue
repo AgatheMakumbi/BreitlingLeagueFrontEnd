b8d3a0afc28a02a66c38f702e667e7efbf46423b
vecval24 <valentine.vecchi@heig-vd.ch>
2025-06-04 15:12:14 +0200

    Page Theory Novelties terminee

--- src/components/LeagueSubPages/NoveltiesTheory.vue
+++ src/components/LeagueSubPages/NoveltiesTheory.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="centered">
         <h1 class="pageTitleHero force-black-text mt-15">History</h1>
-        <p class="centered mt-7 mx-10">
+        <p class="mt-7 mx-10">
             In this presentation, you will discover that although a brand’s reputation is typically built over several
             decades of its history, this is not always the case. For instance, Breitling is often recognized as a brand
             specializing in instruments designed specifically for aviation professionals. This image has indeed been the
             one we aimed to project so far, especially since the quartz crisis, which marked a pivotal moment in the
-            brand’s evolution.
+            brand’s evolution.</p><br>
+        <p class="mt-7 mx-10">
             The History module offers a comprehensive exploration of the brand and its diverse worlds throughout time.
             We will examine the brand’s story in broad strokes to help you understand the foundations and legitimacy of
             our current positioning, while also providing you with the knowledge to strengthen your sales pitch and
-            better connect with customers.
+            better connect with customers.</p><br>
+        <p class="mt-7 mx-10">
             The journey began in 1884 with the founding of the brand by Léon Breitling, whose ambition was to create the
             most precise and reliable time measurement instruments available. This legacy of innovation and quality has
             been preserved and built upon for more than a century, continuing to this day.
@@ -18,10 +20,55 @@
             and respected watch brands worldwide. The three generations of the founding family—Léon, Gaston, and
             Willy—shared a common mission: to develop the world’s finest chronograph. Their dedication and passion laid
             the groundwork for the brand’s enduring success and prestigious reputation in the watchmaking industry.
-        </p>    
+        </p>
+
+        <div class="w-96 h-60 relative mt-8 mb-15">
+            <div class="w-full h-44 top-[53px] absolute bg-yellow-500"></div>
+            <img class="w-80 h-50 mt-5 top-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"
+                src="/src/assets/imgMontre.png" />
+        </div>
+        <p class="mx-10 mb-15">Building on this rich heritage, Breitling has continually pushed the boundaries of
+            innovation and design.
+            Each generation introduced new technologies and crafted timepieces that not only met the highest standards
+            of precision but also reflected the evolving needs of professionals and enthusiasts alike. From pioneering
+            aviator watches to sophisticated chronographs, the brand’s commitment to excellence has remained unwavering.
+            Today, Breitling combines this tradition with modern innovation, ensuring that every watch carries forward
+            the spirit of its founders while appealing to a new generation of wearers who value both performance and
+            style. This ongoing evolution is what keeps Breitling at the forefront of the watchmaking world.
+        </p>
+        <div class="w-80 h-36 relative">
+            <img class="w-36 h-36 object-cover absolute left-0 top-0 shadow-[0px_4px_4px_rgba(0,0,0,0.25)]"
+                src="/src/assets/imgMontre2.jpg" />
+            <img class="w-36 h-36 object-cover left-[166px] top-0 absolute shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"
+                src="/src/assets/imgMontre3.jpg" />
+        </div>
+        <h2 class="pageTitle2 force-black-text mt-15">year 1990 - 2000</h2>
+        <p class="mt-7 mx-10">
+            Between 1990 and 2000, Breitling entered a crucial decade that would redefine its trajectory and solidify
+            its reputation as a premier luxury watchmaker.
+            After the upheaval caused by the quartz crisis in the 1970s and 1980s, Breitling faced the challenge of
+            reconnecting with its roots in mechanical watchmaking while adapting to a rapidly changing market. This
+            period was marked by a strategic return to the brand’s core strengths: precision chronographs, robust
+            engineering, and a strong link to aviation heritage.
+        </p><br>
+        <p class="mt-7 mx-10">
+            Throughout the 1990s, Breitling focused heavily on innovation and craftsmanship. The brand invested in
+            refining its mechanical movements, ensuring that each timepiece met the highest standards of accuracy and
+            reliability. Several new models were launched, blending traditional watchmaking techniques with contemporary
+            design elements that appealed to both long-time enthusiasts and new customers. This era also saw Breitling
+            expanding its product range beyond professional aviation instruments to include elegant watches suitable for
+            everyday wear, broadening its appeal.
+            In addition to product innovation, Breitling made significant strides in global expansion.
+        </p><br>
+        <p class="mt-7 mx-10">
+            The brand increased its presence in key international markets, opening boutiques and strengthening
+            partnerships with retailers worldwide.</p>
+    </div>
+    <div class="centered mt-10 mb-20">
+        <TheButtonPrimary label-name="Mini Game" />
     </div>
 </template>
 <script setup>
-
+import TheButtonPrimary from '@/components/TheButtonPrimary.vue';
 </script>
 <style setup></style>

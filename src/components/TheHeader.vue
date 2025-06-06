88e265a75c1c7a1c072e94fe604adf53b879d901
vecval24 <valentine.vecchi@heig-vd.ch>
2025-06-06 11:37:16 +0200

    Responsive page onboarding

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -35,6 +35,7 @@ const userAvatar = '../assets/avatar.png'
         <RouterLink to="/140-years" class="hover:text-white">Scrolllytelling</RouterLink>
         <RouterLink to="/cup" class="hover:text-white">Breitling Cup</RouterLink>
         <RouterLink to="/league" class="hover:text-white"> Breitling League</RouterLink>
+        <RouterLink to="/league/onboarding" class="hover:text-white">onboarding</RouterLink>
         
       </div>
 

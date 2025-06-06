494e75a10c8ef7ca98590d52e0050254ad0f6588
vecval24 <valentine.vecchi@heig-vd.ch>
2025-06-06 10:31:05 +0200

    Lorem ipsum changes

--- src/components/LeagueSubPages/OpponentChoice.vue
+++ src/components/LeagueSubPages/OpponentChoice.vue
@@ -1,39 +1,30 @@
-<template >
-    <h1 class="pageTitle">Choose the opponent</h1>
-      <p class="pageDescription">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui
-      </p>
-        <!-- Liste scrollable toujours visible -->
-    <ul class="border-3
+<template>
+  <h1 class="pageTitle">Choose the opponent</h1>
+  <p class="pageDescription">
+    Select a colleague from your boutique below to challenge them in a 1v1 quiz duel. Compete on topics of your choice
+    and test your knowledge in a friendly and motivating way. Use the table to find and pick your opponent quickly, then
+    get ready to prove your skills and earn valuable points!
+  </p>
+  <!-- Liste scrollable toujours visible -->
+  <ul class="border-3
     border-yellow-500 rounded-md max-h-64 w-[90%] overflow-y-auto shadow-sm bg-white divide-y">
-      <li
-        v-for="player in players"
-        :key="player.rank"
-        @click="selectPlayer(player)"
-        class="px-4 py-2 hover:bg-gray-100 cursor-pointer flex items-center gap-3"
-        :class="{ 'bg-yellow-500': selectedOpponent?.rank === player.rank }"
-      >
-        <img :src="player.avatar" class="w-6 h-6 rounded-full object-cover" />
-        <div class="flex flex-row justify-between w-full">
-          <div class="text-sm font-medium">
-            {{ player.rank }}. {{ player.pseudo }}
-          </div>
-          <div class="text-xs text-gray-500">
-            {{ player.country }} - {{ player.score }} pts
-          </div>
+    <li v-for="player in players" :key="player.rank" @click="selectPlayer(player)"
+      class="px-4 py-2 hover:bg-gray-100 cursor-pointer flex items-center gap-3"
+      :class="{ 'bg-yellow-500': selectedOpponent?.rank === player.rank }">
+      <img :src="player.avatar" class="w-6 h-6 rounded-full object-cover" />
+      <div class="flex flex-row justify-between w-full">
+        <div class="text-sm font-medium">
+          {{ player.rank }}. {{ player.pseudo }}
         </div>
-      </li>
-    </ul>
-    <p class="errorMessages"></p>
-    
-    <TheButtonPrimary
-      label-name="Next"
-      @click="handleVerification"
-    />
+        <div class="text-xs text-gray-500">
+          {{ player.country }} - {{ player.score }} pts
+        </div>
+      </div>
+    </li>
+  </ul>
+  <p class="errorMessages"></p>
+
+  <TheButtonPrimary label-name="Next" @click="handleVerification" />
 </template>
 <script setup>
 import { onMounted } from "vue"
@@ -129,6 +120,4 @@ const players = [
 ]
 
 </script>
-<style >
-    
-</style>
+<style></style>
